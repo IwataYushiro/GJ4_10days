07d811f4d84c95317cb50eabcb22582870482101
nagahama_kouki <k021g1319@g.neec.ac.jp>
2024-09-04 16:47:05 +0900

    シーン管理と仮のUI終わり

--- Scripts/main.cpp
+++ Scripts/main.cpp
@@ -37,10 +37,25 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	// 画像などのリソースデータの変数宣言と読み込み
-
+	//タイトル画面(タイトル画面からシューティングゲーム)とBGM
+	int titleGraph;
+	titleGraph = LoadGraph("Resources/Textures/title.png");
+	//ゲームシーン画面とBGM
+	int gameUiGraph;
+	gameUiGraph = LoadGraph("Resources/Textures/gemeui.png");
+	//クリア画面(シューティングゲームからクリア画面)とBGM
+	int clearGraph;
+	clearGraph = LoadGraph("Resources/Textures/clear.png");
 
 	// ゲームループで使う変数の宣言
-
+	//シーン管理
+	int title = 0;
+	int gamemode = 1;
+	int clear = 2;
+	int sceneNo = title;
+	
+	//シーン用のタイマー
+	int sceneTimer[3] = {0};
 
 	// 最新のキーボード情報用
 	char keys[256] = { 0 };
@@ -53,15 +68,52 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		// 最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
 		// 最新のキーボード情報を取得
 		GetHitKeyStateAll(keys);
-
 		// 画面クリア
 		ClearDrawScreen();
 		//---------  ここからプログラムを記述  ----------//
 
 		// 更新処理
+		//シーン移行管理
+		if (keys[KEY_INPUT_RETURN] == 1) {
+			if (sceneNo == title) {
+				sceneTimer[title]++;
+				sceneTimer[clear] = 0;
+			}
+			else if (sceneNo == gamemode) {
+				sceneTimer[gamemode]++;
+				sceneTimer[title] = 0;
+			}
+			else if (sceneNo == clear) {
+				sceneTimer[clear]++;
+				sceneTimer[gamemode] = 0;
+			}
+		}
+		if (sceneTimer[title] >= 30) {
+			sceneNo = gamemode;
+		}
+		else if (sceneTimer[gamemode] >= 30) {
+			sceneNo = clear;
+		}
+		else if (sceneTimer[clear] >= 30) {
+			sceneNo = title;
+		}
 
 
 		// 描画処理
+		if (sceneNo == 0) {
+			DrawGraph(0, 0, titleGraph, true);
+		}
+		else if (sceneNo == 1) {
+			DrawGraph(0, 0, gameUiGraph, true);
+		}
+		else if (sceneNo == 2) {
+			DrawGraph(0, 0, clearGraph, true);
+		}
+
+		DrawFormatString(0, 0, GetColor(122, 122, 122), "sceneTimer[title] : %d", sceneTimer[title]);
+		DrawFormatString(0, 30, GetColor(122, 122, 122), "sceneTimer[gamemode] : %d", sceneTimer[gamemode]);
+		DrawFormatString(0, 60, GetColor(122, 122, 122), "sceneTimer[clear] : %d", sceneTimer[clear]);
+
 
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
